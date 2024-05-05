73d50f30a0d05f2eac10afcfbe75db6abd36859d
Mhd.Anggie Rinovka <45094075+MhdAnggieRinovka@users.noreply.github.com>
2024-05-05 07:08:10 +0700

    Clean Code

--- carPage.php
+++ carPage.php
@@ -136,17 +136,8 @@
         <div class="quixnav">
             <div class="quixnav-scroll">
                 <ul class="metismenu" id="menu">
-                    <!-- <li class="nav-label first">Main Menu</li> -->
-                    <!-- <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i
-                                class="icon icon-single-04"></i><span class="nav-text">Dashboard</span></a>
-                        <ul aria-expanded="false">
-                            <li><a href="./index.html">Dashboard 1</a></li>
-                            <li><a href="./index2.html">Dashboard 2</a></li>
-                        </ul>
-                    </li> -->
-                    <!-- <li class="nav-label">Apps</li> -->
                     <li><a href="home.php">Data Client</a></li>
-                    <li><a href="./app-profile.html">Data Car</a></li>
+                    <li><a href="carPage.php">Data Car</a></li>
                 </ul>
             </div>
 
